4164a5036d00ea6b873e832c2703c14d5b364599
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-05 23:01:58 +0800

    pass the assertion exception sample tests

--- src/test/java/com/odde/assertionsample/AssertExceptionSample.java
+++ src/test/java/com/odde/assertionsample/AssertExceptionSample.java
@@ -2,6 +2,7 @@ package com.odde.assertionsample;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.Assert.assertEquals;
 
 public class AssertExceptionSample {
@@ -15,11 +16,10 @@ public class AssertExceptionSample {
     }
 
     @Test
-    public void divide_zero()
-    {
+    public void divide_zero() {
         Calculator calculator = new Calculator();
-        double actual = calculator.divide(5, 0);
 
         //how to assert expected exception?
+        assertThatExceptionOfType(YouShallNotPassException.class).isThrownBy(() -> calculator.divide(5, 0));
     }
 }
